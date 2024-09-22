0554a1bfc6ebdee104beee566fd7302f04b6a610
John Andrei Saldivar <andreisldvr@gmail.com>
2024-09-22 17:52:37 +0800

    Update Exercise #3- Diamond Shape.py

--- Exercise #3- Diamond Shape.py
+++ Exercise #3- Diamond Shape.py
@@ -1,21 +1,13 @@
-# Exercise 3:  Diamond Shape (advance topic):
-
-# Write a Python function named print_diamond that takes an odd integer n as an argument and prints a diamond shape with a width of n using the * character.
-# For n = 5, the output should be:
-#     *
-#   ***
-# *****
-#   ***
-#     *
-# Note: If an even number is passed, the function should return "Please provide an odd integer."
-
+# Function to print a diamond shape
 def print_diamond():
     while True:
         try:
             n = int(input("\nEnter an odd integer as width of a diamond: "))
             if n % 2 != 0:
+                # Print the upper part of the diamond
                 for i in range(n // 2 + 1):
                     print(" " * (n // 2 - i) + "*" * (2 * i + 1))
+                # Print the lower part of the diamond
                 for i in range(n // 2 - 1, -1, -1):
                     print(" " * (n // 2 - i) + "*" * (2 * i + 1))
                 break
@@ -23,5 +15,6 @@ def print_diamond():
                 print("Please provide an odd integer.")
         except ValueError:
             print("Invalid input! Please enter a valid integer.")
-        
+
+# Call the function to print the diamond
 print_diamond()
